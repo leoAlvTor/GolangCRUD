d6787a206334b8168fb3bfe72d3c897075a7132f
Leo Alvarado <45834184+leoAlvTor@users.noreply.github.com>
2022-03-10 22:00:41 -0600

    Function GET now returns a set of rows instead of specific struct (trying to make function more generic).

--- src/controller/DBQueries.go
+++ src/controller/DBQueries.go
@@ -2,28 +2,15 @@ package controller
 
 import (
 	"database/sql"
-	"facturacion/model"
 	"fmt"
 )
 
-func Get(db *sql.DB, query string, params []interface{}) ([]interface{}, error) {
-	var objects []interface{}
+func Get(db *sql.DB, query string, params []interface{}) (rows *sql.Rows) {
 	rows, err := db.Query(query, params...)
 	if err != nil {
-		return nil, fmt.Errorf("error while executing statement: %v", err)
+		return nil
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var album model.Album
-		if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
-			return nil, fmt.Errorf("error while reading rows, error %v", err)
-		}
-		objects = append(objects, album)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("error with rows: %v", err)
-	}
-	return objects, nil
+	return rows
 }
 
 func Add(db *sql.DB, query string, params []interface{}) (int64, error) {
